bab0887aefbdd69a9496c3e84b4215fe1fe0946b
HugoBrisebois <hugo.brisebois@outlook.com>
2026-04-19 13:50:14 -0400

    using example code to test out the driver

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import time
+
 from st7789v.interface import RaspberryPi
 from st7789v import BufferedDisplay
+
+
+with RaspberryPi() as rpi:
+
+# init the display
+display = BufferedDisplay(rpi, rotation=270)
+
+# show black buffer (empty buffer)
+display.update
+time.sleep(0.5)
+
+# draw a blue rectangle
+display.draw.rectangle((0, 0, 320, 240), fill='blue')
+
+# update only the top halfof the screen
+display.update_partial(0, 0, 320, 120)
