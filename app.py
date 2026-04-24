86af71e1389f441b7703a5888d172942931af157
HugoBrisebois <hugo.brisebois@outlook.com>
2026-04-24 12:19:35 -0400

    finalized the temperature implementation and displayed it on the flask server return

--- app.py
+++ app.py
@@ -49,24 +49,11 @@ print(f"log: {lon}")
 # calling the openweather API to get the temperature
 r1 = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_API}")
 response1 = r1
-data = response1.json()
-city1 = data[0]
+data1 = response1.json()
+print(data1)
+temp1 = data1["main"]["temp"]
 
-if response1:
-    print(city1["lat"])
-    print(city1["lon"])
-
-    # store the latitudes and longetitudes
-    lon = city1["lon"]
-    lat = city1["lat"]
-    
-
-print(f"lat: {lat}")
-print(f"log: {lon}")
-
-# store returned temperature
-if response1:
-    print(temp1["main"])
+print(temp1)
 
 
 
@@ -78,5 +65,5 @@ def hello_world():
     return f"""
     <p>Hello, World!</p>
     <H4>Temperature</H4>
-    
+    {temp1 - 273.15, 1}
     """
