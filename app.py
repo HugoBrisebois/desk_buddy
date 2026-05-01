6cc44beabd2fec3bed6e00eda4d38ded9d919907
HugoBrisebois <hugo.brisebois@outlook.com>
2026-05-01 13:38:26 -0400

    made some spelling corrections and made sure that all the variable were defined

--- app.py
+++ app.py
@@ -3,95 +3,73 @@ from flask_apscheduler import APScheduler
 from flask import Flask, render_template
 import requests
 
+# Global time variable updated by the scheduler
+current_time = datetime.now().strftime("%H:%M")
 
 
 def get_location():
-    # start up
-    # init the api keys needed and the application and global VARs
-    # location VARs
-    city = ""
-    state = ""
-    country = ""
-
-    # weather API (OpenWeather API key)
-    # get city, state and country info
     city = input("Please enter the name of your city: ")
     state = input("Please enter the name of your state: ")
-    country = input("Please enter the name if your country: ")
+    country = input("Please enter the name of your country: ")
+    weather_API = input("Please enter your OpenWeather API key: ")
 
-    weather_API = ""
-    weather_API = input("Please enter your OpenWeather: ")
-
-    r = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={weather_API}")
-    response = r.text
-
-    lat = ""
-    lon = ""
-        
-    # Store the API data 
-    response = r
-    data = response.json()
+    r = requests.get(
+        f"http://api.openweathermap.org/geo/1.0/direct"
+        f"?q={city},{state},{country}&limit=1&appid={weather_API}"
+    )
+    data = r.json()
     city1 = data[0]
 
-    if response:
-        # store the latitudes and longetitudes
-        lon = city1["lon"]
-        lat = city1["lat"]
-    
-    print(f"lat: {lat}")
-    print(f"log: {lon}")
+    lat = city1["lat"]
+    lon = city1["lon"]
 
+    print(f"lat: {lat}")
+    print(f"lon: {lon}")
     return lat, lon, weather_API
 
 
 def get_temp(lat, lon, weather_API):
-    # calling the OpenWeather API to get the temperature
-    r1 = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_API}")
+    r1 = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?lat={lat}&lon={lon}&appid={weather_API}"
+    )
     data1 = r1.json()
-    print(data1)
-    
-    # store the temperature in VARs
     temp1 = data1["main"]["temp"]
-
     return temp1
 
 
 def get_weather(lat, lon, weather_API):
-    # calling the OpenWeather API to get the temperature
-    r1 = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_API}")
+    r1 = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?lat={lat}&lon={lon}&appid={weather_API}"
+    )
     data1 = r1.json()
-    
-    # store weather in a VAR
     weather = data1["weather"][0]["description"]
-
     return weather
 
-# get time in a function
-def get_time():
-    current_time = datetime.now().strftime("%H:%M")
 
-    print(current_time)
-    return current_time
-
-
-# getting all the VARs stored with the Parsed returned data to display on the widgets
-lat, lon, weather_API = get_location()
-temp1 = get_temp(lat, lon, weather_API)
-weather = get_weather(lat,lon, weather_API)
-
-# Setting up the Schedueler
-schedueler = APScheduler()
-
-@schedueler.task('interval', id='get_time', seconds=30, misfire_grace_time=900)
-
-
-# setting up the web app 
+# App and scheduler setup
 app = Flask(__name__)
+scheduler = APScheduler()
+
+
+@scheduler.task('interval', id='update_time', seconds=30, misfire_grace_time=900)
+def update_time():
+    global current_time
+    current_time = datetime.now().strftime("%H:%M")
+    print(f"Scheduler tick: {current_time}")
+
 
 @app.route('/')
 def index():
-    time1 = get_time()
-    return render_template('index.html', time_of_day=time1)
-        
+    return render_template('index.html', time_of_day=current_time)
 
 
+if __name__ == '__main__':
+    lat, lon, weather_API = get_location()
+    temp1 = get_temp(lat, lon, weather_API)
+    weather = get_weather(lat, lon, weather_API)
+
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run(debug=True)
